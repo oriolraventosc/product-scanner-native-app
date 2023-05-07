1658cb106b496ed075667b8e1513644b7b0e70d6
oriolraventosc <oriol.raventos.c@gmail.com>
2023-05-07 19:51:18 +0200

    Setup redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { store } from "./src/redux/store";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 };
 
